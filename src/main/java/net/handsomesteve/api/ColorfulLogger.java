484cf31d141282afdadda77ea1fb7cf1036d279a
handsome-steve <51927714+handsome-steve@users.noreply.github.com>
2024-08-16 10:29:16 +0930

    Updated to Version 1.1.0
    The Library is now instanced as a singleton.

--- src/main/java/net/handsomesteve/api/ColorfulLogger.java
+++ src/main/java/net/handsomesteve/api/ColorfulLogger.java
@@ -11,22 +11,28 @@ import org.slf4j.LoggerFactory;
 import java.util.Objects;
 
 /**
- * Package {@link net.handsomesteve.api}
- * <h2>Colorful Logger
- * <p>public final class
- * <p>This class aims at providing a simple, yet robust way, of adding colors to the foreground and background of your debug console when creating your Minecraft Mods.
- * <p>This is achieved by utilizing pre-configured ANSI codes provided as an {@link Enum}.
+ * <p>Package {@link net.handsomesteve.api}</p>
+ *
+ * <h2>Colorful Logger</h2>
+ * <p>{@code public final class}</p>
+ * <p>
+ * A utility class for logging with ANSI colors in Minecraft mod development environments.
+ * Provides methods to output colored messages to the console, helping in debugging and log differentiation.
+ * This class uses ANSI codes to add color configurations to the log output and ensures singleton access
+ * to maintain a unified logging interface across the mod.
+ * </p>
+ * <p>There are ANSI codes provided as {@link Enum}.</p>
  *
  * <h3>Typical usage pattern:
  *
- * <pre><font color="orange">  import com.handsomesteve.api.ColorfulLogger;
- * import com.handsomesteve.api.ansi.AnsiColorBackground;
- * import com.handsomesteve.api.ansi.AnsiColorText;</font>
+ * <pre><font color="orange">  import net.handsomesteve.api.ColorfulLogger;
+ * import net.handsomesteve.api.ansi.AnsiColorBackground;
+ * import net.handsomesteve.api.ansi.AnsiColorText;</font>
  *
  * public class FabricMod implements ModInitializer {
  *
  *      public static final String MOD_ID = "your-mod-id";
- *      <font color="orange">public static final ColorfulLogger LOGGER = new ColorfulLogger("your-mod-id", false);</font>
+ *      <font color="orange">public static final ColorfulLogger LOGGER = ColorfulLogger.getInstance("your-mod-id", true);</font>
  *
  *      &#64Override
  *      public void onInitialize() {
@@ -40,22 +46,37 @@ import java.util.Objects;
  * }
  * </pre>
  *
- * <p>Please consider that this is a work in progress. Users are welcome contribute ideas or pull requests to the github repository: <a href="https://github.com/handsome-steve/colorfulloggerlib">https://github.com/handsome-steve/colorfulloggerlib</a> </p>
+ * <p>
+ * Please consider that this is a work in progress. Users are welcome contribute ideas or pull requests to the github repository:
+ * <a href="https://github.com/handsome-steve/colorfulloggerlib">https://github.com/handsome-steve/colorfulloggerlib</a>
+ * </p>
+ *
+ * @see AnsiColor
+ * @see AnsiColorText
+ * @see AnsiColorBackground
+ *
  * @author handsome-steve
- * @version 1.0.2
+ * @version 1.1.0
  * @since 1.0.0
  */
 
 public final class ColorfulLogger
 {
+
     /**
-     * <p>Initializes {@link org.slf4j.Logger} as a {@code public final LOGGER} variable to be set by the constructor.
+     * Singleton instance of the logger.
+     */
+    private static ColorfulLogger instance;
+
+    /**
+     * The primary logger used for output.
      */
     private final Logger LOGGER;
 
     /**
-     * <p>Getter method for the class declared private final variable {@code LOGGER}.
-     * @return Value of the private final variable {@code LOGGER} as type {@link Logger}.
+     * Retrieves the current logger instance.
+     *
+     * @return the active Logger instance
      */
     public Logger getLogger()
     {
@@ -63,44 +84,94 @@ public final class ColorfulLogger
     }
 
     /**
-     * <p>Initializes {@code debug} as a {@code private} {@link Boolean} variable to be set by the constructor.
+     * Flag to indicate if debugging is enabled.
      */
     private boolean debug;
+
     /**
-     * <p>Getter method for the class declared private variable {@code debug}.
-     * @return Value of the private variable {@code debug} as type {@link Boolean}.
+     * Checks if debugging is enabled.
+     *
+     * @return true if debugging is enabled, false otherwise.
      */
     public boolean getDebug() { return this.debug; }
+
     /**
-     * <p>Setter method for the class declared private variable {@code debug}.
-     * @param value Takes the new value of the private variable {@code debug} as a {@link Boolean}.
+     * Sets the debugging state.
+     *
+     * @param value true to enable debugging, false to disable.
      */
     public void setDebug(boolean value) { this.debug = value; }
 
     /**
-     * <p>Initial constructor of {@link ColorfulLogger}
-     * @param modId This parameter takes a {@link String} as a value, this is usually the Minecraft Mod's {@code MOD_ID}.
+     * Private constructor for creating a logger instance with a mod identifier.
+     *
+     * @param modId the mod identifier used to create the logger
      */
-    public ColorfulLogger(@NotNull String modId)
+    private ColorfulLogger(@NotNull String modId)
     {
         this.LOGGER = Objects.requireNonNull(LoggerFactory.getLogger(modId));
         this.debug = true;
     }
 
     /**
-     * <p>Overload constructor of {@link ColorfulLogger}
-     * @param modId This parameter takes a {@link String} as a value, this is usually the Minecraft Mod's {@code MOD_ID}.
-     * @param hideDebug This parameter takes a {@link Boolean} as a value to determine whether the {@link ColorfulLogger} should hide its console output.
+     * Private constructor for creating a logger instance with a mod identifier and a debug flag.
+     *
+     * @param modId the mod identifier used to create the logger
+     * @param showDebug true to enable debugging output, false otherwise
      */
-    public ColorfulLogger(@NotNull String modId, boolean hideDebug)
+    private ColorfulLogger(@NotNull String modId, boolean showDebug)
     {
         this.LOGGER = Objects.requireNonNull(LoggerFactory.getLogger(modId));
-        this.debug = !hideDebug;
+        this.debug = showDebug;
     }
 
     /**
-     * <p>Prints out a plain message via the internal {@link org.slf4j.Logger} without any coloring applied to the console.
-     * @param message Takes the message to be printed out as a {@link String}.
+     * Retrieves the singleton logger instance for the specified mod ID. If no instance exists, it creates one.
+     *
+     * @param modId the unique identifier for the mod, must not be null
+     * @return the singleton ColorfulLogger instance
+     * @throws NullPointerException if {@code modId} is null, with an explanation in the error message
+     */
+    @NotNull
+    public static synchronized ColorfulLogger getInstance(@NotNull String modId)
+    {
+        if(instance == null) {
+            instance = new ColorfulLogger(
+                    Objects.requireNonNull(
+                            modId,
+                            "[ERROR]: Parameter 'modId' cannot be null at '@ColorfulLogger.getInstance(String)'."
+                    )
+            );
+        }
+        return instance;
+    }
+
+    /**
+     * Retrieves or creates a singleton logger instance with a specific debug setting for the specified mod ID.
+     *
+     * @param modId the mod identifier, cannot be null
+     * @param showDebug true enables debugging, false disables it
+     * @return the singleton ColorfulLogger instance
+     * @throws NullPointerException if {@code modId} is null, with an error message specifying the issue
+     */
+    @NotNull
+    public static synchronized ColorfulLogger getInstance(@NotNull String modId, boolean showDebug)
+    {
+        if(instance == null) {
+            instance = new ColorfulLogger(
+                    Objects.requireNonNull(
+                            modId,
+                            "[ERROR]: Parameter 'modId' cannot be null at '@ColorfulLogger.getInstance(String, boolean)'."
+            ),
+            showDebug);
+        }
+        return instance;
+    }
+
+    /**
+     * Prints out a plain message via the internal {@link org.slf4j.Logger} without any coloring applied to the console.
+     *
+     * @param message the message to log
      */
     public void info(String message)
     {
@@ -109,7 +180,8 @@ public final class ColorfulLogger
     }
 
     /**
-     * <p>Overload method that prints out a colorful message via the internal {@link org.slf4j.Logger} by coloring the foreground only.
+     * <Overload method that prints out a colorful message via the internal {@link org.slf4j.Logger} by coloring the foreground only.
+     *
      * @param message Takes the message to be printed out as a {@link String}.
      * @param ansiColorText Takes an ANSI color value from {@link AnsiColorText}.
      */
@@ -120,7 +192,8 @@ public final class ColorfulLogger
     }
 
     /**
-     * <p>Overload method that prints out a colorful message via the internal {@link org.slf4j.Logger} by coloring the foreground and background.
+     * <Overload method that prints out a colorful message via the internal {@link org.slf4j.Logger} by coloring the foreground and background.
+     *
      * @param message Takes the message to be printed out as a {@link String}.
      * @param ansiColorText Takes an ANSI color value from {@link AnsiColorText}.
      * @param ansiColorBackground Takes an ANSI color value from {@link AnsiColorBackground}.
